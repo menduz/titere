224dfdd015507c4df722bb109b13e11ee926d69b
menduz <agustin.mz.92@gmail.com>
2018-03-28 12:30:21 -0300

    feat: add files

---
+++ src/lib/index.ts
@@ -0,0 +1,293 @@
+import path = require('path')
+import util = require('util')
+import puppeteer = require('puppeteer')
+import { getLocalPath } from './downloadChromium'
+import { initMocha } from './mochaBrowserShim'
+
+declare var window: any
+
+/**
+ * Options to run the mocha-headless-chrome runner.
+ */
+export interface Options {
+  /**
+   * Arguments to pass Puppeteer, if any.
+   */
+  args?: string[]
+
+  /**
+   * Chrome executable path, if not the system default.
+   */
+  executablePath?: string
+
+  /**
+   * Path or URL of the page which contains tests.
+   */
+  file: string
+
+  /**
+   * Viewport height.
+   */
+  height?: number
+
+  /**
+   * Mocha reporter name (defaults to "Spec").
+   */
+  reporter?: string
+
+  /**
+   * Viewport width.
+   */
+  width?: number
+
+  /**
+   * Test timeout in ms.
+   */
+  timeout?: number
+
+  /**
+   * Whether to show the Chrome window.
+   */
+  visible?: boolean
+}
+
+/**
+ * Description of a runner run.
+ */
+export interface Run {
+  /**
+   * Exposed coverage results.
+   */
+  coverage: object | undefined
+
+  /**
+   * Test results.
+   */
+  result: Result
+}
+
+/**
+ * Test results from a run.
+ */
+export interface Result {
+  /**
+   * Tests that failed.
+   */
+  failures: TestDescription[]
+
+  /**
+   * Tests that passed.
+   */
+  passed: TestDescription[]
+
+  /**
+   * Tests that were pending at completion time.
+   */
+  pending: TestDescription[]
+
+  /**
+   * Test statistics.
+   */
+  stats: ResultStats
+
+  /**
+   * All tests that were run.
+   */
+  tests: TestDescription[]
+}
+
+/**
+ * Description of a single test's results.
+ */
+export interface TestDescription {
+  /**
+   * How many milliseconds it took to run the test.
+   */
+  duration: number
+
+  /**
+   * Any details for an error that happened during the test, if any.
+   */
+  err: TestError
+
+  /**
+   * Full title of the test.
+   */
+  fullTitle: string
+
+  /**
+   * Friendly title of the test.
+   */
+  title: string
+}
+
+export type TestError = Error | {}
+
+/**
+ * Test statistics from a run result.
+ */
+export interface ResultStats {
+  /**
+   * How many milliseconds it took to run the tests.
+   */
+  duration: number
+
+  /**
+   * ISO string formatted end time.
+   */
+  end: string
+
+  /**
+   * How many tests failed.
+   */
+  failures: number
+
+  /**
+   * How many tests passed.
+   */
+  passes: number
+
+  /**
+   * How many tests were still pending at completion time.
+   */
+  pending: number
+
+  /**
+   * ISO string formatted start time.
+   */
+  start: string
+
+  /**
+   * How many tests were run.
+   */
+  tests: number
+}
+
+async function configureViewport(width: number, height: number, page: puppeteer.Page) {
+  if (!width && !height) return page
+
+  let viewport = page.viewport()
+  width && (viewport.width = width)
+  height && (viewport.height = height)
+
+  await page.setViewport(viewport)
+
+  return page
+}
+
+function handleConsole(msg: any) {
+  const args = msg._args
+
+  // tslint:disable-next-line
+  Promise.all(args.map((arg: any) => arg.jsonValue())).then((args: any[]) => {
+    // process stdout stub
+    let isStdout = args[0] === 'stdout:'
+
+    if (isStdout) {
+      // tslint:disable-next-line
+      args = args.slice(1)
+    }
+
+    // tslint:disable-next-line
+    let msg = (util.format as any)(...args)
+    !isStdout && (msg += '\n')
+    process.stdout.write(msg)
+  })
+}
+
+function prepareUrl(filePath: string) {
+  if (/^[a-zA-Z]+:\/\//.test(filePath)) {
+    // path is URL
+    return filePath
+  }
+
+  // local path
+  let resolvedPath = path.resolve(filePath)
+  return `file://${resolvedPath}`
+}
+
+/**
+ * Runs client-side mocha tests in the command line through Puppeteer.
+ *
+ * @returns A Promise for the test results.
+ */
+export async function run({
+  file,
+  reporter,
+  timeout,
+  width,
+  height,
+  args,
+  executablePath,
+  visible
+}: Options): Promise<Run> {
+  // validate options
+  if (!file) {
+    throw new Error('Test page path is required.')
+  }
+
+  let theTimeout = timeout || 60000
+
+  const url = prepareUrl(file)
+
+  const options = {
+    ignoreHTTPSErrors: true,
+    headless: false,
+    args,
+    dumpio: true,
+    executablePath
+  }
+
+  if (!options.executablePath) {
+    executablePath = await getLocalPath()
+  }
+
+  const browser = await puppeteer.launch(options)
+  const page = await browser.newPage()
+
+  await configureViewport(width || 800, height || 600, page)
+
+  page.on('console', handleConsole)
+  page.on('dialog', dialog => dialog.dismiss())
+
+  // tslint:disable-next-line:no-console
+  page.on('pageerror', err => console.error(err))
+
+  await page.evaluateOnNewDocument(initMocha, reporter)
+  await page.goto(url)
+  await page.waitForFunction(() => window['__mochaResult__'], { timeout: theTimeout })
+  const result = await page.evaluate(() => window['__mochaResult__'])
+
+  await browser.close()
+
+  return result
+}
+
+export function serveIndex(compiledTestsLocation: string) {
+  return function(req: any, res: any) {
+    res.writeHead(200, 'OK', {
+      'Content-Type': 'text/html'
+    })
+
+    res.write(`<!DOCTYPE html>
+      <html>
+
+      <head>
+        <title>Mocha Tests</title>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="node_modules/mocha/mocha.css">
+      </head>
+
+      <body>
+        <div id="mocha"></div>
+        <script src="node_modules/mocha/mocha.js"></script>
+        <script>mocha.setup('bdd');</script>
+        <script src="${compiledTestsLocation}"></script>
+      </body>
+
+      </html>
+    `)
+
+    res.end()
+  }
+}
---
+++ src/lib/mochaBrowserShim.ts
@@ -0,0 +1,121 @@
+declare var window: any
+
+/**
+ * This function runs inside the browser.
+ * @param reporter
+ */
+export function initMocha(reporter: keyof typeof Mocha.reporters) {
+  console.log = (console => {
+    const log = console.log.bind(console)
+    return (...args: any[]) => (args.length ? log(...args) : log(''))
+  })(console)
+
+  function shimMochaInstance(m: Mocha) {
+    m.setup({ reporter: Mocha.reporters[reporter] || Mocha.reporters.Spec })
+
+    const run = m.run.bind(m)
+
+    m.run = () => {
+      const all: any[] = []
+      const pending: any[] = []
+      const failures: any[] = []
+      const passes: any[] = []
+
+      function error(err: any) {
+        if (!err) return {}
+
+        let res: any = {}
+        Object.getOwnPropertyNames(err).forEach(key => (res[key] = err[key]))
+        return res
+      }
+
+      function clean(test: Mocha.ITest & { err?: any }) {
+        return {
+          title: test.title,
+          fullTitle: test.fullTitle(),
+          duration: test.duration,
+          err: test.err && error(test.err)
+        }
+      }
+
+      function result(stats?: Mocha.IStats) {
+        return {
+          result: {
+            stats: {
+              tests: all.length,
+              passes: passes.length,
+              pending: pending.length,
+              failures: failures.length,
+              start: stats && stats.start && stats.start.toISOString(),
+              end: stats && stats.end && stats.end.toISOString(),
+              duration: stats && stats.duration
+            },
+            tests: all.map(clean),
+            pending: pending.map(clean),
+            failures: failures.map(clean),
+            passes: passes.map(clean)
+          },
+          coverage: window['__coverage__']
+        }
+      }
+
+      function setResult(this: Mocha.IRunner) {
+        !window['__mochaResult__'] && (window['__mochaResult__'] = result(this.stats))
+      }
+
+      const runner = run(() => setTimeout(() => setResult.call(runner), 0))
+        .on('pass', (test: any) => {
+          passes.push(test)
+          all.push(test)
+        })
+        .on('fail', (test: any) => {
+          failures.push(test)
+          all.push(test)
+        })
+        .on('pending', (test: any) => {
+          pending.push(test)
+          all.push(test)
+        })
+        .on('end', setResult)
+
+      return runner
+    }
+  }
+
+  function shimMochaProcess(M: any) {
+    // Mocha needs a process.stdout.write in order to change the cursor position.
+    !M.process && (M.process = {})
+    !M.process.stdout && (M.process.stdout = {})
+
+    // tslint:disable-next-line:no-console
+    M.process.stdout.write = (data: any) => console.log('stdout:', data)
+    M.reporters.Base.useColors = true
+    M.reporters.none = function None(runner: Mocha.IRunner) {
+      M.reporters.Base.call(this, runner)
+    }
+  }
+
+  Object.defineProperty(window, 'mocha', {
+    get: function() {
+      return undefined
+    },
+    set: function(m) {
+      shimMochaInstance(m)
+      delete window['mocha']
+      window['mocha'] = m
+    },
+    configurable: true
+  })
+
+  Object.defineProperty(window, 'Mocha', {
+    get: function() {
+      return undefined
+    },
+    set: function(m) {
+      shimMochaProcess(m)
+      delete window['Mocha']
+      window['Mocha'] = m
+    },
+    configurable: true
+  })
+}
